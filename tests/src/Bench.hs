2e0ed124aadcaec2434dbdff6fc61a8c4f39c5b8
Andreas Abel <andreas.abel@ifi.lmu.de>
2024-04-08 20:27:41 +0200

    Build with criterion >= 1

--- tests/src/Bench.hs
+++ tests/src/Bench.hs
@@ -9,20 +9,12 @@ import qualified Data.Text.Encoding as TE
 
 import Control.DeepSeq
 import Criterion.Main
-import Criterion.Config (defaultConfig, Config(..), ljust)
-
 
 import System.Environment (getArgs, withArgs)
 
 import qualified NLP.Tokenize.String as StrTok
 import qualified NLP.Tokenize.Text as TextTok
 
-
-myConfig :: Config
-myConfig = defaultConfig {
-              -- Always GC between runs.
-              cfgPerformGC = ljust True
-            }
 
 readF :: FilePath -> IO Text
 readF file = do
@@ -37,14 +29,14 @@ main = do
     (f:rest) -> do
       plugTxt <- mapM readF [f]
       let plugStr = map T.unpack plugTxt
-      deepseq plugStr $ withArgs rest $ defaultMainWith myConfig (return ())
-            [ bgroup "tokenizing"
-              [ bench "Native String Tokenizer" $ nf (map StrTok.tokenize) plugStr
-              , bench "Native Text Tokenizer" $ nf (map TextTok.tokenize) plugTxt
-              , bench "Text->Text based on String Tokenizer" $ nf (map strTokenizer) plugTxt
-              , bench "String->String based on Text Tokenizer" $ nf (map txtTokenizer) plugStr
-              ]
-            ]
+      deepseq plugStr $ withArgs rest $ defaultMain
+        [ bgroup "tokenizing"
+          [ bench "Native String Tokenizer" $ nf (map StrTok.tokenize) plugStr
+          , bench "Native Text Tokenizer" $ nf (map TextTok.tokenize) plugTxt
+          , bench "Text->Text based on String Tokenizer" $ nf (map strTokenizer) plugTxt
+          , bench "String->String based on Text Tokenizer" $ nf (map txtTokenizer) plugStr
+          ]
+        ]
 
 strTokenizer :: Text -> [Text]
 strTokenizer txt = map T.pack (StrTok.tokenize $ T.unpack txt)
